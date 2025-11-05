db0d3037938851abb73d0a7d8d2af973395f3faa
Tiago095 <161241506+Tiago095@users.noreply.github.com>
2025-11-05 11:42:15 +0000

    Upload dos Ficheiros Novos

---
+++ AutoMatch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AutoMatch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
