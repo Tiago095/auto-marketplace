3b356e60fdbfd8eb68aab265806d94f6e137cc41
Tiago Silva <al81175@alunos.utad.pt>
2025-11-05 11:39:06 +0000

    Correçao

--- AutoMatch/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AutoMatch.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
